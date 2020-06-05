2a0dc2955ad2702d00d54fa4273de1de6737db3c
Timur <sver40ck@gmail.com>
2020-06-05 13:47:28 -0400

    added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAcl0RrVq5wq7nk0OZ6Ef1aNPXSWNccmg",
+  authDomain: "trebuno.firebaseapp.com",
+  databaseURL: "https://trebuno.firebaseio.com",
+  projectId: "trebuno",
+  storageBucket: "trebuno.appspot.com",
+  messagingSenderId: "773288457462",
+  appId: "1:773288457462:web:1e5fa925d581c8559b28d7",
+  measurementId: "G-W0MGYX2ZCR"
+};
